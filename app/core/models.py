305d097799fc3b965f95469ede9105d90211f70f
Madhav <sharmamadhav208@gmail.com>
2025-01-30 23:53:22 +0530

    Added student model and created user app(for students)

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 # Create your models here.
+
+class StudentManager(BaseUserManager):
+    
+    def create_user(self):
+        student = self.model()
+        student.set_password()
+        student.save(using=self._db)
+
+        return student
+    
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        student = self.create_user(email, password)
+        
+        student.save(using=self._db)
+
+        return student
+
+class Student(AbstractBaseUser,PermissionsMixin):
+
+    application_no=models.BigIntegerField()
+    name=models.CharField(max_length=200)
+
+
+    objects=StudentManager()
+    USERNAME_FIELD='application_no'
